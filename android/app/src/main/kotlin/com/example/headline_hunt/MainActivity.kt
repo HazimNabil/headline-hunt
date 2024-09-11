ab6208168139a2c865e4fc6e57b574a71ffecb05
HazimNabil <HazemNabil95@outlook.com>
2024-09-12 01:01:14 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/headline_hunt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.headline_hunt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HeadlineHunt());
+}
+
+class HeadlineHunt extends StatelessWidget {
+  const HeadlineHunt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
