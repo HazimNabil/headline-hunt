c44e44ee8e057d543e3650b6fdf8ef3290c50852
HazimNabil <HazemNabil95@outlook.com>
2024-09-13 00:21:34 +0200

    Add colors to text styles

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:headline_hunt/core/utils/app_colors.dart';
 import 'package:headline_hunt/core/utils/size_config.dart';
 
 class AppStyles {
@@ -6,21 +7,7 @@ class AppStyles {
     return TextStyle(
       fontSize: SizeConfig.getResponsiveFontSize(context, 18),
       fontWeight: FontWeight.bold,
-    );
-  }
-
-  static TextStyle styleRegular18(BuildContext context) {
-    return TextStyle(
-      fontSize: SizeConfig.getResponsiveFontSize(context, 18),
-      fontWeight: FontWeight.normal,
-    );
-  }
-
-  static TextStyle styleNormal12(BuildContext context) {
-    return TextStyle(
-      fontSize: SizeConfig.getResponsiveFontSize(context, 12),
-      fontWeight: FontWeight.normal,
-      fontStyle: FontStyle.italic,
+      color: AppColors.primaryColor,
     );
   }
 
@@ -28,34 +15,7 @@ class AppStyles {
     return TextStyle(
       fontSize: SizeConfig.getResponsiveFontSize(context, 15),
       fontWeight: FontWeight.bold,
-    );
-  }
-
-  static TextStyle styleMedium15(BuildContext context) {
-    return TextStyle(
-      fontSize: SizeConfig.getResponsiveFontSize(context, 15),
-      fontWeight: FontWeight.w500,
-    );
-  }
-
-  static TextStyle styleMedium14(BuildContext context) {
-    return TextStyle(
-      fontSize: SizeConfig.getResponsiveFontSize(context, 14),
-      fontWeight: FontWeight.w500,
-    );
-  }
-
-  static TextStyle styleBold13(BuildContext context) {
-    return TextStyle(
-      fontSize: SizeConfig.getResponsiveFontSize(context, 13),
-      fontWeight: FontWeight.bold,
-    );
-  }
-
-  static TextStyle styleMedium13(BuildContext context) {
-    return TextStyle(
-      fontSize: SizeConfig.getResponsiveFontSize(context, 13),
-      fontWeight: FontWeight.w500,
+      color: AppColors.primaryColor,
     );
   }
 
@@ -63,6 +23,39 @@ class AppStyles {
     return TextStyle(
       fontSize: SizeConfig.getResponsiveFontSize(context, 15),
       fontWeight: FontWeight.normal,
+      color: Colors.black,
+    );
+  }
+
+  static TextStyle styleBold14(BuildContext context) {
+    return TextStyle(
+      fontSize: SizeConfig.getResponsiveFontSize(context, 14),
+      fontWeight: FontWeight.bold,
+      color: AppColors.primaryColor,
+    );
+  }
+
+  static TextStyle styleMedium14(BuildContext context) {
+    return TextStyle(
+      fontSize: SizeConfig.getResponsiveFontSize(context, 14),
+      fontWeight: FontWeight.w500,
+      color: AppColors.secondaryColor,
+    );
+  }
+
+  static TextStyle styleBold13(BuildContext context) {
+    return TextStyle(
+      fontSize: SizeConfig.getResponsiveFontSize(context, 13),
+      fontWeight: FontWeight.bold,
+      color: AppColors.categoryColor,
+    );
+  }
+
+  static TextStyle styleMedium13(BuildContext context) {
+    return TextStyle(
+      fontSize: SizeConfig.getResponsiveFontSize(context, 13),
+      fontWeight: FontWeight.w500,
+      color: AppColors.secondaryColor,
     );
   }
 }
