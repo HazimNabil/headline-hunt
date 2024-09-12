705cbe55989a79ba0920e89acd08eb079e91b89b
HazimNabil <HazemNabil95@outlook.com>
2024-09-12 20:57:50 +0200

    Remove flutter screenutil package

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -1,50 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppStyles {
-  static final styleBold18 = TextStyle(
-    fontSize: 18.sp,
+  static const styleBold18 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.bold,
   );
 
-  static final styleRegular18 = TextStyle(
-    fontSize: 18.sp,
+  static const styleRegular18 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.normal,
   );
 
-  static final styleNormal12 = TextStyle(
-    fontSize: 12.sp,
+  static const styleNormal12 = TextStyle(
+    fontSize: 12,
     fontWeight: FontWeight.normal,
     fontStyle: FontStyle.italic,
   );
 
-  static final styleBold15 = TextStyle(
-    fontSize: 15.sp,
+  static const styleBold15 = TextStyle(
+    fontSize: 15,
     fontWeight: FontWeight.bold,
   );
 
-  static final styleMedium15 = TextStyle(
-    fontSize: 15.sp,
+  static const styleMedium15 = TextStyle(
+    fontSize: 15,
     fontWeight: FontWeight.w500,
   );
 
-  static final styleMedium14 = TextStyle(
-    fontSize: 14.sp,
+  static const styleMedium14 = TextStyle(
+    fontSize: 14,
     fontWeight: FontWeight.w500,
   );
 
-  static final styleBold13 = TextStyle(
-    fontSize: 13.sp,
+  static const styleBold13 = TextStyle(
+    fontSize: 13,
     fontWeight: FontWeight.bold,
   );
 
-  static final styleMedium13 = TextStyle(
-    fontSize: 13.sp,
+  static const styleMedium13 = TextStyle(
+    fontSize: 13,
     fontWeight: FontWeight.w500,
   );
 
-  static final styleRegular15 = TextStyle(
-    fontSize: 15.sp,
+  static const styleRegular15 = TextStyle(
+    fontSize: 15,
     fontWeight: FontWeight.normal,
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:headline_hunt/core/utils/app_colors.dart';
 
 void main() {
@@ -16,21 +15,15 @@ class HeadlineHunt extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(390, 844),
-      minTextAdapt: true,
-      builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          locale: DevicePreview.locale(context),
-          builder: DevicePreview.appBuilder,
-          theme: ThemeData(
-            primaryColor: AppColors.primaryColor,
-            scaffoldBackgroundColor: AppColors.backgroundColor,
-          ),
-          home: const Scaffold(),
-        );
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData(
+        primaryColor: AppColors.primaryColor,
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+      ),
+      home: const Scaffold(),
     );
   }
 }
