635b715fcd44d85a3365c1495fc95d068e3022f2
HazimNabil <HazemNabil95@outlook.com>
2024-09-12 02:18:38 +0200

    Create app styles class for text styles

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppStyles {
+  static final styleBold18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final styleRegular18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.normal,
+  );
+
+  static final styleNormal12 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.normal,
+    fontStyle: FontStyle.italic,
+  );
+
+  static final styleBold15 = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final styleMedium15 = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final styleMedium14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final styleBold13 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final styleMedium13 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final styleRegular15 = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeight.normal,
+  );
+}
