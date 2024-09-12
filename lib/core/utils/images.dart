9ac26f8b87d04c789a79792148488cbf9d67aede
HazimNabil <HazemNabil95@outlook.com>
2024-09-13 00:37:34 +0200

    Generate asset images

---
+++ lib/core/utils/images.dart
@@ -0,0 +1,5 @@
+abstract class Images {
+  static const String assetsImagesAppLogo = 'assets/images/app_logo.svg';
+
+  static const String assetsImagesTestImage = 'assets/images/test_image.svg';
+}
