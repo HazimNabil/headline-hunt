a8a2667da8bc059b0d16ee646ef40f1b81454ac1
HazimNabil <HazemNabil95@outlook.com>
2024-09-13 20:20:22 +0200

    Add app bar to home view

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:headline_hunt/core/utils/app_colors.dart';
+import 'package:headline_hunt/core/utils/app_styles.dart';
+import 'package:headline_hunt/core/utils/images.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: buildAppBar(context),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: AppColors.backgroundColor,
+      centerTitle: true,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(Images.imagesAppLogo),
+          const SizedBox(width: 10),
+          Text(
+            'Headline Hunt',
+            style: AppStyles.styleBold15(context),
+          ),
+        ],
+      ),
+    );
   }
 }
