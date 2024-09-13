4e2003f0c0604480473df3d0681ceb33bb878562
HazimNabil <HazemNabil95@outlook.com>
2024-09-13 02:38:22 +0200

    Add home view to app router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,12 +1,19 @@
 import 'package:go_router/go_router.dart';
+import 'package:headline_hunt/features/home/presentation/views/home_view.dart';
 import 'package:headline_hunt/features/splash/presentation/views/splash_view.dart';
 
 class AppRouter {
+  static const homePath = '/home';
+
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: homePath,
+        builder: (context, state) => const HomeView(),
       ),
     ],
   );
