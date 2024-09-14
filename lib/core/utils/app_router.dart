55cd09c1cb15225483c8dddae74d83587455b4c9
HazimNabil <HazemNabil95@outlook.com>
2024-09-15 00:02:20 +0200

    Add navigation to details view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
+import 'package:headline_hunt/features/home/presentation/views/details_view.dart';
 import 'package:headline_hunt/features/home/presentation/views/home_view.dart';
 import 'package:headline_hunt/features/splash/presentation/views/splash_view.dart';
 
 class AppRouter {
   static const homePath = '/home';
+  static const detailsPath = '/details';
 
   static final router = GoRouter(
     routes: [
@@ -14,6 +16,10 @@ class AppRouter {
       GoRoute(
         path: homePath,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: detailsPath,
+        builder: (context, state) => const DetailsView(),
       ),
     ],
   );
--- lib/features/home/presentation/views/widgets/article_tile.dart
+++ lib/features/home/presentation/views/widgets/article_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:headline_hunt/core/utils/app_colors.dart';
+import 'package:headline_hunt/core/utils/app_router.dart';
 import 'package:headline_hunt/core/utils/images.dart';
 
 import 'article_tile_info.dart';
@@ -11,14 +13,17 @@ class ArticleTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-            Image.asset(Images.imagesTestImage),
-            const SizedBox(width: 10),
-            const Expanded(
-              child: ArticleTileInfo(),
-            ),
-          ],
+        GestureDetector(
+          onTap: () => context.push(AppRouter.detailsPath),
+          child: Row(
+            children: [
+              Image.asset(Images.imagesTestImage),
+              const SizedBox(width: 10),
+              const Expanded(
+                child: ArticleTileInfo(),
+              ),
+            ],
+          ),
         ),
         const SizedBox(height: 30),
         const Divider(
