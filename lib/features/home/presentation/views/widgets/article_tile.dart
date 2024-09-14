b632fa29b683cf13be194528096daa0f44265d2f
HazimNabil <HazemNabil95@outlook.com>
2024-09-14 02:38:04 +0200

    Add divider to article tile

--- lib/features/home/presentation/views/widgets/article_tile.dart
+++ lib/features/home/presentation/views/widgets/article_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:headline_hunt/core/utils/app_colors.dart';
 import 'package:headline_hunt/core/utils/images.dart';
 
 import 'article_tile_info.dart';
@@ -8,12 +9,20 @@ class ArticleTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Image.asset(Images.imagesTestImage),
-        const SizedBox(width: 10),
-        const Expanded(
-          child: ArticleTileInfo(),
+        Row(
+          children: [
+            Image.asset(Images.imagesTestImage),
+            const SizedBox(width: 10),
+            const Expanded(
+              child: ArticleTileInfo(),
+            ),
+          ],
+        ),
+        const SizedBox(height: 30),
+        const Divider(
+          color: AppColors.secondaryBackgroundColor,
         ),
       ],
     );
