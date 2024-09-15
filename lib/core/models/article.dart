aba5ec19cdc3415db2f60a28a482b80251dfa7b3
HazimNabil <HazemNabil95@outlook.com>
2024-09-15 03:57:02 +0200

    Create article model

---
+++ lib/core/models/article.dart
@@ -0,0 +1,68 @@
+import 'package:equatable/equatable.dart';
+
+class Article extends Equatable {
+  final String id;
+  final String title;
+  final String description;
+  final String url;
+  final String author;
+  final String image;
+  final String language;
+  final String category;
+  final DateTime published;
+
+  const Article({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.author,
+    required this.image,
+    required this.language,
+    required this.category,
+    required this.published,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      url: json['url'] as String,
+      author: json['author'] as String,
+      image: json['image'] as String,
+      language: json['language'] as String,
+      category: json['category'][0] as String,
+      published: DateTime.parse(json['published'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'url': url,
+      'author': author,
+      'image': image,
+      'language': language,
+      'category': category,
+      'published': published,
+    };
+  }
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      title,
+      description,
+      url,
+      author,
+      image,
+      language,
+      category,
+      published,
+    ];
+  }
+}
