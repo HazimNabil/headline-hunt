354083b94a8daee37c9f019661ed5e80327a9e01
HazimNabil <HazemNabil95@outlook.com>
2024-09-16 22:30:10 +0200

    Create latest news cubit method

--- lib/features/home/presentation/manager/latest_news_cubit/latest_news_cubit.dart
+++ lib/features/home/presentation/manager/latest_news_cubit/latest_news_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:headline_hunt/core/models/article.dart';
+import 'package:headline_hunt/features/home/data/repos/home_repo.dart';
 
 part 'latest_news_state.dart';
 
 class LatestNewsCubit extends Cubit<LatestNewsState> {
-  LatestNewsCubit() : super(LatestNewsInitial());
+  final HomeRepo homeRepo;
+
+  LatestNewsCubit(this.homeRepo) : super(LatestNewsInitial());
+
+  void fetchLatestNews(String category) async {
+    emit(LatestNewsLoading());
+    var result = await homeRepo.fetchLatestNews(category);
+    result.fold(
+      (failure) => emit(LatestNewsFailure(failure.errorMessage)),
+      (articles) => emit(LatestNewsSuccess(articles)),
+    );
+  }
 }
