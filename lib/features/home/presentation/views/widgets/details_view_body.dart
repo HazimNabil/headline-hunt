b10b7b145bdc3f9acf01a37fa017f38e70076322
HazimNabil <HazemNabil95@outlook.com>
2024-09-18 23:08:02 +0200

    Add error widget to cached network image in details view

--- lib/features/home/presentation/views/widgets/details_view_body.dart
+++ lib/features/home/presentation/views/widgets/details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:headline_hunt/core/models/article.dart';
+import 'package:headline_hunt/core/utils/app_colors.dart';
 import 'package:headline_hunt/features/home/presentation/views/widgets/article_body.dart';
 
 class DetailsViewBody extends StatelessWidget {
@@ -17,6 +18,12 @@ class DetailsViewBody extends StatelessWidget {
           imageUrl: article.image,
           height: screenHeight / 3.5,
           fit: BoxFit.fill,
+          errorWidget: (context, url, error) {
+            return Container(
+              color: AppColors.secondaryColor,
+              child: const Icon(Icons.error),
+            );
+          },
         ),
         Padding(
           padding: const EdgeInsets.all(16),
