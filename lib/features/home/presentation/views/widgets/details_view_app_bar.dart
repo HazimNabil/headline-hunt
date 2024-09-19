73d1ecf87e40c2ab8d512561f4e8135ee9a1773c
HazimNabil <HazemNabil95@outlook.com>
2024-09-19 23:13:45 +0200

    Display bookmark icon based on status in details view app bar

--- lib/features/home/presentation/views/widgets/details_view_app_bar.dart
+++ lib/features/home/presentation/views/widgets/details_view_app_bar.dart
@@ -29,12 +29,23 @@ class DetailsViewAppBar extends StatelessWidget implements PreferredSizeWidget {
         ),
       ),
       actions: [
-        IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset(Images.imagesBookmarkUnselected),
+        Padding(
+          padding: const EdgeInsets.only(right: 16.0),
+          child: getBookmarkIcon(),
         ),
       ],
     );
+  }
+
+  SvgPicture getBookmarkIcon() {
+    final String bookmarkIcon;
+    final isBookmarked = homeRepo.isBookmarked(article.id);
+    if (isBookmarked) {
+      bookmarkIcon = Images.imagesBookmarkSelected;
+    } else {
+      bookmarkIcon = Images.imagesBookmarkUnselected;
+    }
+    return SvgPicture.asset(bookmarkIcon);
   }
 
   @override
