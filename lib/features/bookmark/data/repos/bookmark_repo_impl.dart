7115b4ad02d1a26e50f029ee5ee18ac278305249
HazimNabil <HazemNabil95@outlook.com>
2024-09-21 03:45:43 +0200

    Implement book mark repo

---
+++ lib/features/bookmark/data/repos/bookmark_repo_impl.dart
@@ -0,0 +1,28 @@
+import 'package:headline_hunt/core/models/article.dart';
+import 'package:headline_hunt/core/utils/hive_service.dart';
+import 'package:headline_hunt/features/bookmark/data/repos/bookmark_repo.dart';
+
+class BookmarkRepoImpl implements BookmarkRepo {
+  final HiveService _hiveService;
+
+  BookmarkRepoImpl(this._hiveService);
+
+  @override
+  List<Article> getBookmarkedArticles() {
+    return _hiveService.readArticles();
+  }
+
+  @override
+  bool isBookmarked(String articleId) {
+    return _hiveService.isInDatabase(articleId);
+  }
+
+  @override
+  void toggleBookmark(Article article) {
+    if (isBookmarked(article.id)) {
+      _hiveService.removeArticle(article.id);
+    } else {
+      _hiveService.addArticle(article);
+    }
+  }
+}
