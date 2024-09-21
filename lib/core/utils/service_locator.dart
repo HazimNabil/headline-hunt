c8f7ae0c3eb861ba029104d79a790d4add71b026
HazimNabil <HazemNabil95@outlook.com>
2024-09-21 03:50:08 +0200

    Add book repo impl to service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -3,6 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:headline_hunt/core/models/article.dart';
 import 'package:headline_hunt/core/utils/api_service.dart';
 import 'package:headline_hunt/core/utils/hive_service.dart';
+import 'package:headline_hunt/features/bookmark/data/repos/bookmark_repo_impl.dart';
 import 'package:headline_hunt/features/home/data/repos/home_repo_impl.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -14,7 +15,15 @@ void setupServiceLocator() {
   );
 
   locator.registerSingleton(
-    HiveService(Hive.box<Article>(HiveService.boxName)),
+    HiveService(
+      Hive.box<Article>(HiveService.boxName),
+    ),
+  );
+
+  locator.registerSingleton(
+    BookmarkRepoImpl(
+      locator.get<HiveService>(),
+    ),
   );
 
   locator.registerSingleton(
