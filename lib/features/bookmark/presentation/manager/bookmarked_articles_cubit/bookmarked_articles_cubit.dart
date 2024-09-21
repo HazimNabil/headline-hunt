f0d5558d60e943dd434667f3f0f38325f4811e48
HazimNabil <HazemNabil95@outlook.com>
2024-09-21 06:33:36 +0200

    Create fetch bookmarked articles cubit method

--- lib/features/bookmark/presentation/manager/bookmarked_articles_cubit/bookmarked_articles_cubit.dart
+++ lib/features/bookmark/presentation/manager/bookmarked_articles_cubit/bookmarked_articles_cubit.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:headline_hunt/core/models/article.dart';
+import 'package:headline_hunt/features/bookmark/data/repos/bookmark_repo.dart';
 
 part 'bookmarked_articles_state.dart';
 
 class BookmarkedArticlesCubit extends Cubit<BookmarkedArticlesState> {
-  BookmarkedArticlesCubit() : super(BookmarkedArticlesInitial());
+  final BookmarkRepo _bookmarkRepo;
+
+  BookmarkedArticlesCubit(this._bookmarkRepo)
+      : super(BookmarkedArticlesInitial());
+
+  void fetchBookmarkedArticles() {
+    final bookmarkedArticles = _bookmarkRepo.getBookmarkedArticles();
+    emit(BookmarkedArticlesLoaded(bookmarkedArticles));
+  }
 }
