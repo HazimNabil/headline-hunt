f7f97906d19885e8979ac8649c1519bfca0d113c
HazimNabil <HazemNabil95@outlook.com>
2024-09-22 01:07:22 +0200

    Add widget in case of no bookmarks

--- lib/features/bookmark/presentation/views/widgets/bookmarked_articles_builder.dart
+++ lib/features/bookmark/presentation/views/widgets/bookmarked_articles_builder.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:headline_hunt/core/widgets/article_list.dart';
 
 import '../../manager/bookmarked_articles_cubit/bookmarked_articles_cubit.dart';
+import 'no_bookmarks_widget.dart';
 
 class BookmarkedArticlesBuilder extends StatelessWidget {
   const BookmarkedArticlesBuilder({super.key});
@@ -12,6 +13,9 @@ class BookmarkedArticlesBuilder extends StatelessWidget {
     return BlocBuilder<BookmarkedArticlesCubit, BookmarkedArticlesState>(
       builder: (context, state) {
         final loadedState = state as BookmarkedArticlesLoaded;
+        if (loadedState.bookmarkedArticles.isEmpty) {
+          return const NoBookmarksWidget();
+        }
         return ArticleList(
           articles: loadedState.bookmarkedArticles,
         );
---
+++ lib/features/bookmark/presentation/views/widgets/no_bookmarks_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:headline_hunt/core/utils/app_styles.dart';
+import 'package:headline_hunt/core/utils/images.dart';
+
+class NoBookmarksWidget extends StatelessWidget {
+  const NoBookmarksWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverFillRemaining(
+      hasScrollBody: false,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              Images.imagesNoData,
+            ),
+            Text(
+              'No bookmarks in this category',
+              style: AppStyles.styleBold18(context),
+            ),
+            const SizedBox(height: 20)
+          ],
+        ),
+      ),
+    );
+  }
+}
