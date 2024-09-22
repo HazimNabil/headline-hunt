658fee1273816c01ffe8546e5774b176da299eca
HazimNabil <HazemNabil95@outlook.com>
2024-09-22 22:04:43 +0200

    Add search news cubit method

--- lib/features/search/presentation/manager/search_news_cubit/search_news_cubit.dart
+++ lib/features/search/presentation/manager/search_news_cubit/search_news_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:headline_hunt/core/models/article.dart';
+import 'package:headline_hunt/features/search/data/repos/search_repo.dart';
 
 part 'search_news_state.dart';
 
 class SearchNewsCubit extends Cubit<SearchNewsState> {
-  SearchNewsCubit() : super(SearchNewsInitial());
+  final SearchRepo _searchRepo;
+
+  SearchNewsCubit(this._searchRepo) : super(SearchNewsInitial());
+
+  Future<void> searchNews(String searchTerm) async {
+    emit(SearchNewsLoading());
+    final result = await _searchRepo.searchNews(searchTerm);
+    result.fold(
+      (failure) => emit(SearchNewsFailure(failure.errorMessage)),
+      (articles) => emit(SearchNewsSuccess(articles)),
+    );
+  }
 }
