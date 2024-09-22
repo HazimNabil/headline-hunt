1eaa5499afae2e0a760ba0b4f0d14230200dafbd
HazimNabil <HazemNabil95@outlook.com>
2024-09-22 20:02:39 +0200

    Add search repo impl to service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -5,6 +5,7 @@ import 'package:headline_hunt/core/utils/api_service.dart';
 import 'package:headline_hunt/core/utils/hive_service.dart';
 import 'package:headline_hunt/features/bookmark/data/repos/bookmark_repo_impl.dart';
 import 'package:headline_hunt/features/home/data/repos/home_repo_impl.dart';
+import 'package:headline_hunt/features/search/data/repos/search_repo_impl.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 var locator = GetIt.instance;
@@ -28,6 +29,12 @@ void setupServiceLocator() {
 
   locator.registerSingleton(
     HomeRepoImpl(
+      locator.get<ApiService>(),
+    ),
+  );
+
+  locator.registerSingleton(
+    SearchRepoImpl(
       locator.get<ApiService>(),
     ),
   );
