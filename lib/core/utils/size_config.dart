e4503e3f5893f2104a8e89c525e01ec04bdeb407
HazimNabil <HazemNabil95@outlook.com>
2025-01-26 16:32:07 +0200

    Fix responsive font size limits issue

--- lib/core/utils/size_config.dart
+++ lib/core/utils/size_config.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 abstract class SizeConfig {
   static double getResponsiveFontSize(BuildContext context, double fontSize) {
-    var width = MediaQuery.sizeOf(context).width;
+    final width = MediaQuery.sizeOf(context).width;
 
-    var scaleFactor = width / 400;
-    var responsiveFontSize = fontSize * scaleFactor;
-    var lowerLimit = responsiveFontSize * 0.8;
-    var upperLimit = responsiveFontSize * 1.20;
+    final scaleFactor = width / 400;
+    final responsiveFontSize = fontSize * scaleFactor;
+    final lowerLimit = fontSize * 0.8;
+    final upperLimit = fontSize * 1.20;
 
     return responsiveFontSize.clamp(lowerLimit, upperLimit);
   }
